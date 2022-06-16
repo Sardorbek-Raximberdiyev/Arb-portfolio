48b460821e965852256a1bd99f8d24272b4e98f9
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-06-16 15:32:40 +0500

    Correction site

--- js/main.js
+++ js/main.js
@@ -4,25 +4,4 @@ const elDarkMoreCheked = document.querySelector('.dark-more__cheked');
 elDarkMoreCheked.addEventListener('click', function () {
   document.body.classList.toggle('dark-mode');
 });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const theme = localStorage.getItem("theme", "dark");
-//   const elDarkMoreCheked = document.getElementById("chk");
-
-//   if (theme === "dark") {
-//     document.documentElement.classList.add("dark-mode");
-//   } else {
-//     document.documentElement.classList.remove("dark-mode");
-//   }
-
-//   elDarkMoreCheked.addEventListener("click", function () {
-//     document.documentElement.classList.toggle("dark-mode");
-
-//     if (document.documentElement.classList.contains("dark-mode")) {
-//       localStorage.setItem("theme", "dark");
-//     } else {
-//       localStorage.setItem("theme", "light");
-//     }
-//   });
-// });
+wadawd
