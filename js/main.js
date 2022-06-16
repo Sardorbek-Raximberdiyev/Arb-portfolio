20d53fa01ad5a878693ed6344ff6b1655529fe10
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-06-16 11:53:50 +0500

    Correction dark mode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-const elDarkMore = document.querySelector('.dark_more');
+const elDarkMore = document.querySelector('.dark-mode');
 const elDarkMoreCheked = document.querySelector('.dark-more__cheked');
 
 elDarkMoreCheked.addEventListener('click', function () {
-  document.body.classList.toggle('dark_more');
+  document.body.classList.toggle('dark-mode');
 });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const theme = localStorage.getItem("theme", "dark");
+//   const elDarkMoreCheked = document.getElementById("chk");
+
+//   if (theme === "dark") {
+//     document.documentElement.classList.add("dark-mode");
+//   } else {
+//     document.documentElement.classList.remove("dark-mode");
+//   }
+
+//   elDarkMoreCheked.addEventListener("click", function () {
+//     document.documentElement.classList.toggle("dark-mode");
+
+//     if (document.documentElement.classList.contains("dark-mode")) {
+//       localStorage.setItem("theme", "dark");
+//     } else {
+//       localStorage.setItem("theme", "light");
+//     }
+//   });
+// });
